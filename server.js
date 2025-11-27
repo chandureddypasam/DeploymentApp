d836afedaf4f95e72fd976fd5690a1f75a1f3bc4
Chandrasekhharreddy <144087291+Chandrasekhharreddy@users.noreply.github.com>
2025-11-27 21:59:50 +0530

    Modified Package.json

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname,"./client/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
