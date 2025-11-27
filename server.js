f2ee8c9d94c221a6bc36721fd5fd74bbfc461152
Chandrasekhharreddy <144087291+Chandrasekhharreddy@users.noreply.github.com>
2025-11-27 21:54:58 +0530

    Changed in Server

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname,"./client/build")));
 
-app.get("*",(res,req)=>{
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 
